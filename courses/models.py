818cb1fe780762eafe8c9958fd9078c68152bc14
stuxbart <stuxbart99@gmail.com>
2020-04-08 20:10:00 +0200

    Add slug generator

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import pre_save
+
+from .utils import slug_generator
 
 User = settings.AUTH_USER_MODEL
 
@@ -9,13 +12,19 @@ class Subject(models.Model):
     slug        = models.SlugField(unique=True)
     owner       = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subjects')
     overview    = models.TextField()
-    # enrolled
     # many owners
+    #access key
     updated     = models.DateTimeField(auto_now=True)
     created     = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+def subject_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slug_generator(instance)
+
+pre_save.connect(subject_pre_save_receiver, sender=Subject)
 
 class Module(models.Model):
     subject     = models.ForeignKey(Subject, on_delete=models.CASCADE)
---
+++ courses/utils.py
@@ -0,0 +1,18 @@
+import random
+import string
+from django.utils.text import slugify
+
+def random_string_generator(length=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(length))
+
+def slug_generator(instance, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title)
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(slug=slug)
+    if qs_exists:
+        new_slug = "{slug}-{randstr}".format(slug=slug, randstr=random_string_generator(length=5))
+        return slug_generator(instance, new_slug=new_slug)
+    return slug
