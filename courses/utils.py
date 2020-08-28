5622beaa9544d57118c9393d8aca3da9a13f38df
stuxbart <stuxbart99@gmail.com>
2020-08-28 14:15:47 +0200

    update slug generator

--- courses/utils.py
+++ courses/utils.py
@@ -2,20 +2,28 @@ import random
 import string
 from django.utils.text import slugify
 
+
 def random_string_generator(length=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(length))
+
 
 def slug_generator(instance, new_slug=None):
     if new_slug is not None:
         slug = new_slug
     else:
-        slug = slugify(instance.title)
+        if hasattr(instance, "title"):
+            slug = slugify(instance.title)
+        elif hasattr(instance, "name"):
+            slug = slugify(instance.name)
+        else:
+            slug = random_string_generator(length=6)
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(slug=slug)
     if qs_exists:
         new_slug = "{slug}-{randstr}".format(slug=slug, randstr=random_string_generator(length=5))
         return slug_generator(instance, new_slug=new_slug)
     return slug
+
 
 def user_index_generator(instance):
     index = random_string_generator(length=6, chars=string.digits)
