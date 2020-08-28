ba9d20125b30bc541d76170a36c33e13c85a5253
stuxbart <stuxbart99@gmail.com>
2020-08-28 14:40:39 +0200

    add category to the admin site

--- courses/admin.py
+++ courses/admin.py
@@ -1,22 +1,28 @@
 from django.contrib import admin
 
-from .models import Course, Module, Content, Text, Image, File, Video
+from .models import Course, Module, Content, Text, Image, File, Video, Category
+
+
 # Register your models here.
 
 class TextInline(admin.StackedInline):
     model = Text
 
+
 class ImageInline(admin.StackedInline):
     model = Image
+
 
 class FileInline(admin.StackedInline):
     model = File
 
+
 @admin.register(Content)
 class ContentAdmin(admin.ModelAdmin):
     model = Content
-    list_display = ['module', 'content_type', 'object_id', 'item']   
-    inlines = [] 
+    list_display = ['module', 'content_type', 'object_id', 'item']
+    inlines = []
+
 
 class ModuleInline(admin.StackedInline):
     model = Module
@@ -24,20 +30,26 @@ class ModuleInline(admin.StackedInline):
 
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ['title','course','description']
+    list_display = ['title', 'course', 'description']
     sortable_by = ['course']
     inlines = []
-    
+
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = [
-            "title",
-            "slug",     
-            "owner",
-            "overview",
-            "updated",
-            "created",
-            ]
+        "title",
+        "slug",
+        "owner",
+        "overview",
+        "updated",
+        "created",
+    ]
     prepopulated_fields = {'slug': ('title',)}
     inlines = [ModuleInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent_category']
+    prepopulated_fields = {'slug': ('name',)}
