e6cdf70531508e7985a37f1778c4409ef3e78b33
stuxbart <stuxbart99@gmail.com>
2020-09-02 23:05:59 +0200

    create content serializers

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Course, Category, Module
+from .models import Course, Category, Module, Text, Content, Image, Video, File
 from accounts.serializers import SnippetUserSerializer
 
 
@@ -37,6 +37,14 @@ class CreateModuleSerializer(serializers.HyperlinkedModelSerializer):
         extra_kwargs = {
             'course': {'required': True},
         }
+
+    def create(self, validated_data):
+        owner = validated_data.get('owner')
+        course = validated_data['course']
+        if owner != course.owner:
+            raise serializers.ValidationError("Owners doesn't match")
+
+        return super().create(validated_data)
 
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
@@ -82,3 +90,75 @@ class DetailCategorySerializer(serializers.HyperlinkedModelSerializer):
 
 class EnrollCourseSerializer(serializers.Serializer):
     access_key = serializers.CharField()
+
+
+class BaseContentSerializer(serializers.ModelSerializer):
+    owner = SnippetUserSerializer(many=False, read_only=True)
+    course = SnippetCourseSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['id', 'title', 'owner', 'course', 'module',
+                  'visible', 'order', 'item', 'created', 'updated']
+
+    def create(self, validated_data):
+        owner = validated_data.get('owner')
+        del validated_data['owner']
+        module = validated_data['module']
+
+        if owner != module.owner:
+            raise serializers.ValidationError("Owners doesn't match")
+
+        if hasattr(self, 'item_class'):
+            ItemClass = getattr(self, 'item_class')
+            item = ItemClass(**validated_data.get('item'), owner=owner)
+            item.save()
+            validated_data['item'] = item
+        else:
+            raise serializers.ValidationError()
+
+        return super().create(validated_data)
+
+
+class TextSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Text
+        fields = ['title', 'content']
+
+
+class TextContentSerializer(BaseContentSerializer):
+    item = TextSerializer(many=False)
+    item_class = Text
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['file']
+
+
+class ImageContentSerializer(BaseContentSerializer):
+    item = ImageSerializer(many=False, read_only=True)
+    item_class = Image
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ['file']
+
+
+class VideoContentSerializer(BaseContentSerializer):
+    item = VideoSerializer(many=False, read_only=True)
+    item_class = Video
+
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ['file']
+
+
+class FileContentSerializer(BaseContentSerializer):
+    item = FileSerializer(many=False, read_only=True)
+    item_class = File
